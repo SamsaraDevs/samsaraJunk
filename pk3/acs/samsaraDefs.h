604f7cc1e4c46c7a808506126a02aa3c97aade93
TerminusEst13 <TerminusEst13@gmail.com>
2013-02-04 20:43:36 -0500

    Samsara_hexenjump > samsara_jumpmod in the JAZZ console. (Not compiled)

--- pk3/acs/samsaraDefs.h
+++ pk3/acs/samsaraDefs.h
@@ -75,7 +75,7 @@ int HELPSTR =
 "Welcome to the Wheel of Samsara! There are a few optional RCon commands you may want to consider.\n\
 samsara_banjetpack 0/1: Toggles whether Duke is allowed to pick up his jetpack or not.\n\
 samsara_nocustomgravity 0/1: Toggles whether the heroes have custom gravity or use only Doom gravity.\n\
-samsara_hexenjump 0/1: Toggles whether the Heroes get a height boost for HeXen play.\n\
+samsara_jumpmod: Gives the indicated multiplier to jump height. 9 for HeXen, -8 for Strife.\n\
 samsara_permault 0/1: Toggles whether the Weapon VII is persistent or vanishes on pickup.\n\
 samsara_lmslife [0-5]: Affects how much health/armor people have on LMS spawn/respawn.\n\
 samsara_lmsult 0/1: Toggles whether players get their VII in LMS.\n\
