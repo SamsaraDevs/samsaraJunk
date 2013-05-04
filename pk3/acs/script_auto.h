3802b199b97ec48e76e9d470264c1d2b5d29b1c8
Ijon Tichy <gztichy@lavabit.com>
2013-05-03 22:44:19 -0500

    oh god nuclear kicks fucking god damn

--- pk3/acs/script_auto.h
+++ pk3/acs/script_auto.h
@@ -283,6 +283,7 @@ script SAMSARA_SYNTHFIRE (int startTime)
 {
     int pln = PlayerNumber();
 
+    while (ServerEnterTimes[pln] == startTime)
     {
         if (keyDown(BT_ATTACK)) { GiveInventory("SynthFireLeft", 1); }
         else { TakeInventory("SynthFireLeft", 0x7FFFFFFF); }
