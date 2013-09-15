d96cd3c9a34a7f76f43b2df8aa1db4f9315eb407
TerminusEst13 <TerminusEst13@gmail.com>
2013-09-14 21:11:17 -0400

    Ranger now has the Rocket Powered Impaler as his instagib rifle. It is the only instagib rifle that allows the user to harmlessly rocketjump through areas for extra mobility. For SPACE TRAVEL, try it with the Quad Damage.

--- pk3/acs/samsaraFuncs.h
+++ pk3/acs/samsaraFuncs.h
@@ -526,6 +526,6 @@ function int HandleInstagib(int respawning)
     if (CheckInventory("HexenClass") == 1) { GiveInventory("Bloodscourge",1); }
     if (CheckInventory("DukeClass") == 1) { GiveInventory("Golden Desert Eagle",1); }
     if (CheckInventory("MarathonClass") == 1) { GiveInventory("SPNKR-25 Auto Cannon",1); }
-    if (CheckInventory("QuakeClass") == 1) { GiveInventory(" Railgun ",1); }
+    if (CheckInventory("QuakeClass") == 1) { GiveInventory("Rocket Powered Impaler",1); }
     return 1;
 }
